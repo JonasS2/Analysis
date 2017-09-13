956f70d28256dcaa87e5d7dd348981af61fc7423
JonasS2 <jonas.schimmer@web.de>
2017-09-13 14:49:53 +0200

    heart isolation dates included

--- Code/SampleSheet.R
+++ Code/SampleSheet.R
@@ -91,6 +91,37 @@ own_sample_sheet$p_t <- "NA"
 own_sample_sheet$p_t[grepl("p",own_sample_sheet$V3)] <- "p"
 own_sample_sheet$p_t[grepl(" t",own_sample_sheet$V3)] <- "t"
 
+
+own_sample_sheet$date_heart_isolation <- "NA"
+own_sample_sheet$date_heart_isolation[grepl("Stim1",own_sample_sheet$Info_treatment_group)] <- "9.03.2016"
+own_sample_sheet$date_heart_isolation[17:48] <- "22.06.2016"
+#rest is inserted from updated sheet per hand
+own_sample_sheet$date_heart_isolation[49:50] <- "18.01.2017"
+own_sample_sheet$date_heart_isolation[51:52] <- "23.02.2017"
+own_sample_sheet$date_heart_isolation[53:54] <- "18.01.2017"
+own_sample_sheet$date_heart_isolation[55] <- "23.02.2017"
+own_sample_sheet$date_heart_isolation[56:58] <- "19.01.2017"
+own_sample_sheet$date_heart_isolation[59:63] <- "22.01.2017"
+own_sample_sheet$date_heart_isolation[64] <- "22.11.2016"
+own_sample_sheet$date_heart_isolation[65:66] <- "28.11.2016"
+own_sample_sheet$date_heart_isolation[67:71] <- "20.12.2016"
+own_sample_sheet$date_heart_isolation[72:73] <- "24.11.2016"
+own_sample_sheet$date_heart_isolation[74] <- "29.11.2016"
+own_sample_sheet$date_heart_isolation[75] <- "21.12.2016"
+own_sample_sheet$date_heart_isolation[76:77] <- "29.11.2016"
+own_sample_sheet$date_heart_isolation[78:79] <- "21.12.2016"
+own_sample_sheet$date_heart_isolation[80] <- "08.12.2016"
+own_sample_sheet$date_heart_isolation[81] <- "29.11.2016"
+own_sample_sheet$date_heart_isolation[82] <- "30.11.2016"
+own_sample_sheet$date_heart_isolation[83] <- "01.12.2016"
+own_sample_sheet$date_heart_isolation[84] <- "19.12.2016"
+own_sample_sheet$date_heart_isolation[85:88] <- "30.11.2016"
+own_sample_sheet$date_heart_isolation[89:91] <- "05.12.2016"
+own_sample_sheet$date_heart_isolation[92] <- "19.12.2016"
+own_sample_sheet$date_heart_isolation[93] <- "05.12.2016"
+own_sample_sheet$date_heart_isolation[94:95] <- "06.12.2016"
+own_sample_sheet$date_heart_isolation[96] <- "19.12.2016"
+
 ################################
 
 saveRDS(own_sample_sheet,"./rds/own_sample_sheet.rds")
